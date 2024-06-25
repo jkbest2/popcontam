f6a7bebac468eac753b3d6f246014c08a0192143
John K Best <John.Best@dfw.wa.gov>
2024-06-25 16:58:40 -0700

    Density-dependent migration occurs early
    Use the number of pre-parr rather than the number of parr migrants as
    the number of fish remaining to rear in the river. Also cleaned up
    comments with some more explanations.

--- R/stillaguamish.R
+++ R/stillaguamish.R
@@ -12,30 +12,30 @@ stilly_sim <- function(pop, nearshore_surv_adj = 1) {
   prespawners <- 0.05 * pop[2] + 0.34 * pop[3] + 0.92 * pop[4] + pop[5]
   ## Prespawn mortality
   spawners <- 0.696937 * prespawners
-  ## Egg production
+  ## Egg production. A fecundity of 5400 is per female, and we assume (as in
+  ## HARP) that returners have an even sex ratio.
   eggs <- beverton_holt(spawners / 2, 5400, 152859370)
   ## Survival to fry
   fry <- eggs * 0.548651
-  # colonization is density-dependent
-  # capacity doubled to allow for two cohorts of fish, non-simultaneously
-  # occupying habitats
+  ## Colonization is density-dependent. Pre-parr are fish that will choose to
+  ## remain in the river to rear. Capacity is doubled to allow for two cohorts
+  ## of fish, non-simultaneously occupying habitats.
   pre_parr <- beverton_holt(fry, 0.302312^(1/9), 1682111.1 * 2)
-  ## Split out fry and and parr migrants
-  # about nine weeks of rearing for parr migrants - based on Snohomish screw
-  # trap data. HARP adjust this to impose a late June temperature penalty
+  ## Parr migrants rear for about nine weeks before moving downstream based on
+  ## Snohomish screw trap data. HARP adjust this to impose a late June
+  ## temperature penalty
   parr_mig <- beverton_holt(pre_parr, 0.302312^(8/9), 1682111.1 * 2)
-  ## Survival to subyearling rearing
-  subyr <- fry * 0.302312^(1/9) # only 1 out of 9 weeks represented
-  ## Using overall parr_mig as the number who stay indicates a constant-ish migration downstream?
-  fry_mig <- (subyr - parr_mig) * 0.128 # migration mortality is estimated
-  ## Subtracting off pre_parr rather than parr_mig seems more consistent with the lcm_chinook model
-  ## fry_mig <- (subyr - pre_parr) * 0.128 # migration mortality is estimated
-  stopifnot(fry_mig >= 0) # Shouldn't be a problem, but the check makes me feel better
+  ## Migration "choice" occurs after the first week, so is based on the number
+  ## of pre-parr. See also line 50 of lcm-chinook.R, where natal_fry are
+  ## subtracted from surviving pre_fry. Migration survival (0.128, estimated) is
+  ## applied at the same time here. This cannot be negative because the fry
+  ## survival here is equal to the maximum possible pre_parr survival above.
+  fry_mig <- (fry * 0.302312^(1/9) - pre_parr) * 0.128
   ## Transition to first marine year
   delta_fry <- beverton_holt(fry_mig, 0.35, 45335)
-  # note that this age structure shouldn't be taken literally - we apply all
-  # mortality effects in the nearshore, when we know that part of it is
-  # attributable to harvest.
+  ## Note that this age structure shouldn't be taken literally - we apply all
+  ## mortality effects in the nearshore, when we know that part of it is
+  ## attributable to harvest.
   age00 <- (delta_fry + parr_mig) * 0.01645436 * nearshore_surv_adj
   age01 <- pop[1] * 0.6
   age02 <- pop[2] * (1 - 0.05) * 0.7
