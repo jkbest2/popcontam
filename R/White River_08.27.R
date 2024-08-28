c2783e10ca95f675da8af333f7c5832b25b50ab6
Maya Faber <mayaa.smolcic@gmail.com>
2024-08-28 13:06:44 -0700

    Added fish mortality to the model and pcb exposure data

--- R/White River_08.27.R
+++ R/White River_08.27.R
@@ -52,11 +52,15 @@ white_sim <- function(pop) {
             fry_mig = unname(fry_mig))
 }
 
+library(tidyverse)
 
+source("R/utils.R")
+source("R/fish-size.R")
 
 white_0 <- eq_pop(white_sim, pop0 = rep(1000, 10))
 
 white_0
+
 
 
 
@@ -73,6 +77,9 @@ yrspawn <- attr(white_0, "yrlspawners")
 sar_yrl <- yrspawn / attr(white_0, "yr_mig")
 sar_yrl
 
+get_eggs <- function(pop) {
+    attr(pop, "eggs")
+  }
 
 ##combined freshwater survival (yearlins and subyearlings combined)
 freshsurv <- (smolts + attr(white_0, "yr_mig"))/get_eggs(white_0)
