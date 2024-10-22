5398b0bd909258a98c1b8011589fb97318797a72
John K Best <John.Best@dfw.wa.gov>
2024-10-22 14:18:54 -0700

    Update Puyallup and White population models

--- R/Puyallup River_08_27.R
+++ R/Puyallup River_08_27.R
@@ -1,40 +1,7 @@
 beverton_holt <- function(n, prod, cap) {
-n * prod / (1 + n * prod / cap)
+  n * prod / (1 + n * prod / cap)
 }
 
-puyallup_sim <- function(pop, nearshore_surv_adj = 1) {
-  prespawners <- c(0.093 * pop[3], 0.647 * pop[4], pop[5])
-  migrantprespawn <- beverton_holt(prespawners, 0.722, 12657408.21) #added in this step as there was capacity associated with holding prespawner
-  holdingprespawn <- beverton_holt(migrantprespawn, 0.872, 729726.40)#added in this step as there was capacity associated with migrant prespawner
-  spawners <- beverton_holt(migrantprespawn, 0.971, 1139372348.62)
-  fecundity_age02 <- spawners[1] *4600/2 # fecundity specific for returning two year old females, assumed sex ratio is 50/50 female/male
-  fecundity_age03 <- spawners[2] *5700/2 # fecundity specific for returning three year old females, assumed sex ratio is 50/50 female/male
-  fecundity_age04 <- spawners[3] *6600/2 # fecundity specific for returning four year old females, assumed sex ratio is 50/50 female/male
-  egg_prod <- sum(fecundity_age02 + fecundity_age03 + fecundity_age04) # summing eggs across spawners
-  eggs <- beverton_holt(egg_prod, 1, 623792859.31) # applying capacity to egg production, using productivity of 1 since fecundity was captured in previous three equations
-  fry <- beverton_holt(eggs, 0.391, 9629410.93)
-  fry_mig <- beverton_holt(fry,0.626 * 0.31 * 0.924 * 0.310, 1558678.02) # capturing fry/parr split (31%/69%)
-  parr_mig <- beverton_holt( fry, 0.626 * 0.69 * 0.35 * 0.551, 308326.34) # capturing fry/parr split (31%/69%)
-  age00 <- beverton_holt(fry_mig + parr_mig, 0.08, 67139801.46) * nearshore_surv_adj ##Not sure if this is where to capture
-  age01 <- beverton_holt(pop[1], 0.591, 963877008.33)
-  age02 <- beverton_holt((1-0.093) * pop[2], 0.710, 20077276.13)
-  age03 <- beverton_holt((1 - 0.647) * pop[3], 0.812, 8809670.23)
-  age04 <- beverton_holt(pop[4], 0.881, 1889294.85)
-  
-
-  structure(c(age00, age01, age02, age03, age04),
-            names = paste0("age0", 0:4),
-            oceanadults = unname(age01 + age02 + age03 + age04),
-            prespawners = unname(prespawners),
-            holdingprespawn = unname(holdingprespawn),
-            migrantprespawn = unname(migrantprespawn),
-            spawners = unname(spawners[1] + spawners[2] + spawners[3]), #spawners coming through as age02, age03, and age04 so summing for model output
-            egg_prod = unname(egg_prod),
-            eggs = unname(eggs),
-            fry = unname(fry),
-            parr_mig = unname(parr_mig),
-            fry_mig = unname(fry_mig))
-}
 
 
 
@@ -49,16 +16,16 @@ puyallup_0
 
 spawners <- get_spawners(puyallup_0)
 
-##Smolt to adult returns
+## Smolt to adult returns
 smolts <- attr(puyallup_0, "parr_mig") + attr(puyallup_0, "fry_mig")
 sar_0 <- get_spawners(puyallup_0) / smolts
 sar_0
 smolts
 get_eggs <- function(pop) {
-    attr(pop, "eggs")
-  }
-##Calculate freshwater survival
-freshsurv <- smolts/get_eggs(puyallup_0)
+  attr(pop, "eggs")
+}
+## Calculate freshwater survival
+freshsurv <- smolts / get_eggs(puyallup_0)
 freshsurv
 
 ## Ocean adults
@@ -71,7 +38,8 @@ mort_qreg <- function(pcb) {
   ifelse(
     pcb < 0.100,
     0,
-    pmax(0.1702 + 0.221 * log10(pcb), 0))
+    pmax(0.1702 + 0.221 * log10(pcb), 0)
+  )
 }
 
 growth_qreg <- function(pcb) {
@@ -82,48 +50,51 @@ growth_qreg <- function(pcb) {
   )
 }
 
-##added in adjusted lipid regression
+## added in adjusted lipid regression
 
 mort_qreg_lipid <- function(pcb) {
   ## Effect threshold is 100 ng/g (ww) (2200 ng/g lw), so return zero effect below this
   ifelse(
-    pcb < 3.7, #need to confirm the limit of applicability for lipid weight
+    pcb < 3.7, # need to confirm the limit of applicability for lipid weight
     0,
-    pmax(-0.1253 + 0.221 * log10(pcb), 0))
+    pmax(-0.1253 + 0.221 * log10(pcb), 0)
+  )
 }
 
 
 pcb_data <- read.csv("data/PCB_Puyallup_monitoring_data.csv")
 
-dexposure_fun_2 <- function(concentrations) {   ##not sure i need to do this if I have a pre-generated exposure
-  means <- log(concentrations)  
-  sdlogs <- rep(0.5, length(concentrations))  # Set a default standard deviation value  
-  props <- rep(1/length(concentrations), length(concentrations))  # Equal proportions for each concentration  
+dexposure_fun_2 <- function(concentrations) { ## not sure i need to do this if I have a pre-generated exposure
+  means <- log(concentrations)
+  sdlogs <- rep(0.5, length(concentrations)) # Set a default standard deviation value
+  props <- rep(1 / length(concentrations), length(concentrations)) # Equal proportions for each concentration
 
   function(xs) {
-    vapply(xs, \(x) sum(props * dlnorm(x, means, sdlogs)),  
-           0.0)
+    vapply(
+      xs, \(x) sum(props * dlnorm(x, means, sdlogs)),
+      0.0
+    )
   }
 }
-  
-# Wet weight PCB concentrations  
+
+# Wet weight PCB concentrations
 concentrations <- pcb_data$pcb_uggwet
-  
-dexposure_ww <- dexposure_fun_2(concentrations)  
 
-# Wet weight PCB concentrations with sample-specific lipid corrections 
+dexposure_ww <- dexposure_fun_2(concentrations)
+
+# Wet weight PCB concentrations with sample-specific lipid corrections
 concentrations_lw_sample <- pcb_data$pcb_ugglw_sample
-  
-dexposure_lw <- dexposure_fun_2(concentrations_lw_sample)  
 
-# # Wet weight PCB concentrations using 1% lipid corrections 
+dexposure_lw <- dexposure_fun_2(concentrations_lw_sample)
+
+# # Wet weight PCB concentrations using 1% lipid corrections
 concentrations_lw_1 <- pcb_data$pcb_ugglw_1
-  
-dexposure_lw_1 <- dexposure_fun_2(concentrations_lw_1)  
+
+dexposure_lw_1 <- dexposure_fun_2(concentrations_lw_1)
 
 ## Calculate direct mortality rate based on the exposure distribution and the
 ## Berninger and Tillitt direct mortality relationship
-morts_ww <- integrate(\(pcb) dexposure_ww(pcb) * mort_qreg(pcb), lower = 0, upper = Inf)$value  
+morts_ww <- integrate(\(pcb) dexposure_ww(pcb) * mort_qreg(pcb), lower = 0, upper = Inf)$value
 morts_ww
 ## Apply direct mortality to the to-ocean 0.0 transition (decreasing survival)
 ## ug/g wet weight
@@ -132,11 +103,11 @@ spawner_mort <- get_spawners(puyallup_mort)
 oa_mort <- get_oceanadults(puyallup_mort)
 red_mort <- 1 - (oa_0 - oa_mort) / oa_0
 red_mort
-Percent_spawner_reduction <- (spawner_mort - spawners)/spawners * 100
+Percent_spawner_reduction <- (spawner_mort - spawners) / spawners * 100
 Percent_spawner_reduction
 
-##ug/g lipid weight (sample specific lipids)
-morts_lw <- integrate(\(pcb) dexposure_lw(pcb) * mort_qreg_lipid(pcb), lower = 0, upper = Inf)$value  
+## ug/g lipid weight (sample specific lipids)
+morts_lw <- integrate(\(pcb) dexposure_lw(pcb) * mort_qreg_lipid(pcb), lower = 0, upper = Inf)$value
 morts_lw
 ## Apply direct mortality to the to-ocean 0.0 transition (decreasing survival)
 ## ug/g wet weight
@@ -145,11 +116,11 @@ spawner_mort <- get_spawners(puyallup_mort)
 oa_mort <- get_oceanadults(puyallup_mort)
 red_mort <- 1 - (oa_0 - oa_mort) / oa_0
 red_mort
-Percent_spawner_reduction <- (spawner_mort - spawners)/spawners * 100
+Percent_spawner_reduction <- (spawner_mort - spawners) / spawners * 100
 Percent_spawner_reduction
 
-##ug/g lipid weight (1% lipids)
-morts_lw_1 <- integrate(\(pcb) dexposure_lw_1(pcb) * mort_qreg_lipid(pcb), lower = 0, upper = Inf)$value  
+## ug/g lipid weight (1% lipids)
+morts_lw_1 <- integrate(\(pcb) dexposure_lw_1(pcb) * mort_qreg_lipid(pcb), lower = 0, upper = Inf)$value
 morts_lw_1
 ## Apply direct mortality to the to-ocean 0.0 transition (decreasing survival)
 ## ug/g wet weight
@@ -158,5 +129,5 @@ spawner_mort <- get_spawners(puyallup_mort)
 oa_mort <- get_oceanadults(puyallup_mort)
 red_mort <- 1 - (oa_0 - oa_mort) / oa_0
 red_mort
-Percent_spawner_reduction <- (spawner_mort - spawners)/spawners * 100
+Percent_spawner_reduction <- (spawner_mort - spawners) / spawners * 100
 Percent_spawner_reduction
--- R/white.R
+++ R/white.R
@@ -1,52 +1,84 @@
 white_sim <- function(pop) {
-  prespawners <- c(0.093 * pop[4], 0.647 * pop[5], pop[6])
-  spawners <- 0.838 * 0.867 * 0.971 * prespawners
-  fecundity <- c(4600, 5700, 6600)
-  eggs <- sum(fecundity * spawners / 2)
-  fry <- eggs * 0.441
-  fry_mig <-  0.16 * 0.599 * 0.803 * 0.356 * fry
-  parr_mig <-  0.72 * 0.599 * 0.240 * 0.563 * fry
-  yr_mig <- 0.12 * 0.599 * 0.596 * 0.604 * 0.959 * fry
-  age00 <- 0.356 * fry_mig + 0.563 * parr_mig
-  age01 <- 0.757 * pop[1] + 0.082 * pop[2]
-  age02 <- 0.590 * pop[3]
-  age03 <- 0.710 * (1 - 0.093) * pop[4]
-  age04 <- 0.812 * (1 - 0.647) * pop[5]
+  ## Find number of spawners
+  prespawners <- c(
+    ## Age 0.* contributions
+    0.093 * pop[3], 0.647 * pop[4], pop[5],
+    ## Age 1.* contributions
+    0.093 * pop[8], 0.647 * pop[9], pop[10]
+  )
+  ## Added migrant and holding prespawners so that the relevant capacities can
+  ## be included.
+  migrantprespawn <- beverton_holt(prespawners, 0.837, 22972996.51)
+  holdingprespawn <- beverton_holt(migrantprespawn, 0.867, 680441.18)
+  spawners <- beverton_holt(migrantprespawn, 0.971, 522439767.58)
 
-  structure(c(yr_mig, age00, age01, age02, age03, age04),
-            names = c("yr_mig", paste0("age0", 0:4)),
-            oceanadults = age01 + age02 + age03 + age04,
-            prespawners = prespawners,
-            spawners = spawners,
-            eggs = eggs,
-            fry = fry,
-            fry_migr = fry_mig,
-            parr_migr = parr_mig)
+  ## Calculate number of eggs; Fecundity is age dependent and assumes 50/50 sex
+  ## ratio
+  fecundity <- c(
+    age02 = spawners[1] * 4600 / 2,
+    age03 = spawners[2] * 5700 / 2,
+    age04 = spawners[3] * 6600 / 2,
+    age12 = spawners[4] * 4600 / 2,
+    age13 = spawners[5] * 5700 / 2,
+    age14 = spawners[6] * 6600 / 2
+  )
+  egg_prod <- sum(fecundity)
+  ## Account for density dependence in egg production
+  eggs <- beverton_holt(egg_prod, 1, 411853741.56)
+
+  ## Find number in each (fry, parr, yearling) life history
+  fry <- beverton_holt(eggs, 0.441, 10212032.14)
+  ## Assume 88% subyearlings, 16% of which are fry migrants
+  fry_mig <- beverton_holt(fry, 0.599 * 0.88 * 0.16 * 0.803 * 0.356, 920795.40)
+  ## Assume 88% subyearlings, 72% of which are parr migrant
+  parr_mig <- beverton_holt(fry, 0.599 * 0.88 * 0.72 * 0.240 * 0.563, 153767.53)
+  # Assume 12% yearling migrants. Freshwater rearing stages are split because
+  # they have separate capacities
+  yr_0agerear <- beverton_holt(fry, 0.599 * 0.12 * 0.596, 768048.17)
+  yr_0agemig <- beverton_holt(yr_0agerear, 0.900, 184719306.08)
+  yr_0ageinac <- beverton_holt(yr_0agemig, 0.604, 970597.87)
+  yr_1agerear <- beverton_holt(yr_0ageinac, 0.959, 6436546.88)
+  yr_mig <- beverton_holt(yr_1agerear, 0.757, 197943007.58)
+
+  ## Calculate the number at each age
+  ## Subyearling migrants
+  age00 <- beverton_holt(fry_mig + parr_mig, 0.08, 60660147.66)
+  age01 <- beverton_holt(pop[1], 0.590, 687697841.82)
+  age02 <- beverton_holt((1 - 0.093) * pop[2], 0.731, 14835699.80)
+  age03 <- beverton_holt((1 - 0.647) * pop[3], 0.849, 6838144.42)
+  ## TODO Check on this mortality if all are expected to return
+  age04 <- beverton_holt(pop[4], 0.882, 1890200.90)
+  ## Yearling migrants
+  age10 <- beverton_holt(yr_mig, 0.0978, 30203352.87)
+  age11 <- beverton_holt(pop[6], 0.590, 660837621.60)
+  age12 <- beverton_holt((1 - 0.093) * pop[7], 0.731, 14092487.28)
+  age13 <- beverton_holt((1 - 0.647) * pop[8], 0.849, 6940007.42)
+  ## TODO Check on this mortality if all are expected to return
+  age14 <- beverton_holt(pop[9], 0.882, 1648312.88)
+
+  structure(
+    c(
+      age00, age01, age02, age03, age04,
+      age10, age11, age12, age13, age14
+    ),
+    names = paste0("age0", 0:4),
+    ## TODO Check on how to count "adults" for this model
+    oceanadults = unname(
+      age01 + age02 + age03 + age04 +
+        age10 + age11 + age12 + age13 + age14
+    ),
+    prespawners = unname(prespawners),
+    holdingprespawn = unname(holdingprespawn),
+    migrantprespawn = unname(migrantprespawn),
+    spawners = unname(sum(spawners)),
+    subyearspawners = unname(sum(spawners[1:3])),
+    yrlspawners = unname(spawners[4:6]),
+    egg_prod = unname(egg_prod),
+    eggs = unname(eggs),
+    eggssubyr = unname(sum(fecundity[1:3])),
+    fry = unname(fry),
+    parr_mig = unname(parr_mig),
+    yr_mig = unname(yr_mig),
+    fry_mig = unname(fry_mig)
+  )
 }
-
-pop <- rep(1e3, 6)
-for (t in 2:300) {
-  pop0 <- pop
-  pop <- white_sim(pop)
-}
-
-
-return <- c(0, 0, 0, 0.093, 0.647, 1)
-spawners <- 0.838 * 0.867 * 0.971 * return
-fecund <- c(0, 0, 0, 4600 / 2, 5700 / 2, 6600 / 2)
-eggs <- spawners * fecund
-fry <- 0.441 * eggs
-fry_mig <- 0.88 * 0.16 * 0.599 * 0.803 * 0.356 * fry
-parr_mig <- 0.88 * 0.72 * 0.599 * 0.240 * 0.563 * fry
-
-yr_mig <- 0.12 * 0.599 * 0.596 * 0.604 * 0.959 * fry
-age00 <- 0.356 * fry_mig + 0.563 * parr_mig
-age01 <- c(0.757, 0.082, 0, 0, 0, 0)
-age02 <- c(0, 0, 0.590, 0, 0, 0)
-age03 <- c(0, 0, 0, 0.710 * (1 - 0.093), 0, 0)
-age04 <- c(0, 0, 0, 0, 0.812 * (1 - 0.647), 0)
-
-white_mat <- rbind(yr_mig, age00, age01, age02, age03, age04)
-colnames(white_mat) <- rownames(white_mat)
-
-white_mat %*% rep(1e3, 6)
