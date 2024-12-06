2f408f7f4c2452f886149c93c94d0ed55071c8f8
John K Best <John.Best@dfw.wa.gov>
2024-12-06 13:35:09 -0800

    Add option to "remove" PCBs

--- R/22-puyallup-pcb-effects.R
+++ R/22-puyallup-pcb-effects.R
@@ -7,25 +7,43 @@ source("R/pcb-effects.R")
 
 post <- read_rds("data/puyallup/pcb_exposure.rds")
 
-pcb_effect <- function(exp_meanlog, exp_sdlog, wt_type) {
-  exp_surv <- function(pcb) {
-    dlnorm(pcb, exp_meanlog, exp_sdlog) *
-      (1 - combo_mort(pcb, wt_type = wt_type))
+pcb_effect <- function(
+    pop_meanlog, pop_sdlog, wt_type, remove_pcbs = TRUE,
+    rel.tol = .Machine$double.eps^0.5, subdivisions = 100) {
+  expected_surv <- function(pcb) {
+    dlnorm(pcb, pop_meanlog, pop_sdlog) *
+      combo_surv(pcb, wt_type = wt_type, remove_pcbs = remove_pcbs)
   }
   integrate(
-    exp_surv,
+    expected_surv,
     lower = 0, upper = Inf,
     ## The default rel.tol occasionally gives incorrect results, including one
     ## set of population parameters that consistently shows an increase in
     ## survival due to PCB exposure!
-    rel.tol = .Machine$double.eps^0.5
+    rel.tol = rel.tol,
+    subdivisions = subdivisions
   )$value
 }
 rv_pcb_effect <- rfun(pcb_effect)
 
-eff_ww <- rv_pcb_effect(post$ww$pop_meanlog, post$ww$pop_sdlog, "ww")
-eff_lw <- rv_pcb_effect(post$lw$pop_meanlog, post$lw$pop_sdlog, "lw")
-eff_lw1 <- rv_pcb_effect(post$lw1$pop_meanlog, post$lw1$pop_sdlog, "lw")
+eff_ww <- rv_pcb_effect(
+  pop_meanlog = post$ww$pop_meanlog,
+  pop_sdlog = post$ww$pop_sdlog,
+  wt_type = "ww",
+  remove_pcbs = TRUE
+)
+eff_lw <- rv_pcb_effect(
+  pop_meanlog = post$lw$pop_meanlog,
+  pop_sdlog = post$lw$pop_sdlog,
+  wt_type = "lw",
+  remove_pcbs = TRUE
+)
+eff_lw1 <- rv_pcb_effect(
+  pop_meanlog = post$lw1$pop_meanlog,
+  pop_sdlog = post$lw1$pop_sdlog,
+  wt_type = "lw",
+  remove_pcbs = TRUE
+)
 
 write_rds(
   list(
