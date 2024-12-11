fe3abac05fc1349f089e9ba45f06905b2a30b5aa
John K Best <John.Best@dfw.wa.gov>
2024-12-11 08:33:44 -0800

    Use spawners instead of ocean adults

--- R/31-puyallup-pop.R
+++ R/31-puyallup-pop.R
@@ -9,14 +9,20 @@ source("R/puyallup.R")
 puy0 <- eq_pop(
   puyallup_sim,
   nearshore_surv_adj = 1,
-  pop0 = rep(1000, 5)
+  pop0 = rep(1000, 5),
+  n_max = 500L
 )
-puy0_oa <- get_oceanadults(puy0)
+puy0_sp <- get_spawners(puy0)
 
 eff <- read_rds("data/puyallup/puy_pcb_eff.rds")
 
 puyallup_exposed <- function(ns_surv, stage = NULL) {
-  eqp <- eq_pop(puyallup_sim, nearshore_surv_adj = ns_surv, pop0 = rep(1000, 5))
+  eqp <- eq_pop(
+    puyallup_sim,
+    nearshore_surv_adj = ns_surv,
+    pop0 = rep(1000, 5),
+    n_max = 500L
+  )
   ## Don't always want the population age structure
   if (!is.null(stage)) {
     eqp <- attr(eqp, stage)
@@ -25,27 +31,27 @@ puyallup_exposed <- function(ns_surv, stage = NULL) {
 }
 rv_puyallup_exposed <- rfun(puyallup_exposed)
 
-puy_oa_ww <- rv_puyallup_exposed(eff$ww, "oceanadults")
-puy_oa_lw <- rv_puyallup_exposed(eff$lw, "oceanadults")
-puy_oa_lw1 <- rv_puyallup_exposed(eff$lw1, "oceanadults")
+puy_sp_ww <- rv_puyallup_exposed(eff$ww, "spawners")
+puy_sp_lw <- rv_puyallup_exposed(eff$lw, "spawners")
+puy_sp_lw1 <- rv_puyallup_exposed(eff$lw1, "spawners")
 
 write_rds(
   list(
-    oa0 = puy0_oa,
-    ww = puy_oa_ww,
-    lw = puy_oa_lw,
-    lw1 = puy_oa_lw1
+    sp = puy0_sp,
+    ww = puy_sp_ww,
+    lw = puy_sp_lw,
+    lw1 = puy_sp_lw1
   ),
-  "data/puyallup/puy_pcb_oceanadults.rds"
+  "data/puyallup/puy_pcb_spawners.rds"
 )
 
 puy_eff <- tibble(
   type = c("Wet", "Lipid", "1% Lipid"),
   eff = c(eff$ww, eff$lw, eff$lw1),
-  oceanadults = c(puy_oa_ww, puy_oa_lw, puy_oa_lw1),
-  oa0 = puy0_oa
+  spawners = c(puy_sp_ww, puy_sp_lw, puy_sp_lw1),
+  sp0 = puy0_sp
 ) |>
   mutate(
-    oa_change = (oceanadults - oa0) / oa0
+    sp_change = (spawners - sp0) / sp0
   )
 write_rds(puy_eff, "data/puyallup/puy_pcb_eff_df.rds")
